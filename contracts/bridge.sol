5cc4e067138c6642026647397f76f5e25e6ebbdf
Yuriy5672 <yuriypestovsky@gmail.com>
2022-05-05 00:49:35 +0300

    create a structure 22-05-05

---
+++ contracts/bridge.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./openzeppline/access/AccessControl.sol";
+import "./openzeppline/security/Pausable.sol";
+
+/**
+
+*/
+
+contract Bridge is AccessControl, Pausable {
+
+}
